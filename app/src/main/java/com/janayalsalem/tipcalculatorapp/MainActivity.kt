c550bc66a4600b55b1fa031d98d8a383d942706b
Jana <Jana.Yousef.ruh.k@tuwaiq.edu.sa>
2022-05-18 12:21:27 +0300

    - Add the result in the header

--- app/src/main/java/com/janayalsalem/tipcalculatorapp/MainActivity.kt
+++ app/src/main/java/com/janayalsalem/tipcalculatorapp/MainActivity.kt
@@ -3,21 +3,28 @@ package com.janayalsalem.tipcalculatorapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.janayalsalem.tipcalculatorapp.ui.theme.TipCalculatorAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppTheme {
-
+            AppTheme{
+                TopHeader()
             }
         }
     }
@@ -33,10 +40,38 @@ fun AppTheme(content: @Composable () -> Unit) {
     }
 }
 
+
+@Composable
+fun TopHeader(totalPerPers: Double = 0.0) {
+
+    val totalFormat = "%.2f".format(totalPerPers)
+
+    Surface(color = Color(0xFFe9d7f7)
+        ,modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .padding(12.dp)
+            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+    ) {
+        Column(modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+
+            Text(text = "Total Per Person", style = MaterialTheme.typography.subtitle1)
+            Text(text = "\$$totalFormat", style = MaterialTheme.typography.h4)
+
+        } // End Column
+
+    } // End Surface
+
+}
+
+
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     AppTheme{
-
+        TopHeader()
     }
 }
