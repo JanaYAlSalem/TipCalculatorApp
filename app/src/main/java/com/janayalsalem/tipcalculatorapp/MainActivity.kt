4734893fb3e0e04caf5fc7b11ddf37b181a2ef0f
Jana <Jana.Yousef.ruh.k@tuwaiq.edu.sa>
2022-05-19 12:42:36 +0300

    - Add Split Section

--- app/src/main/java/com/janayalsalem/tipcalculatorapp/MainActivity.kt
+++ app/src/main/java/com/janayalsalem/tipcalculatorapp/MainActivity.kt
@@ -12,9 +12,10 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -24,6 +25,7 @@ import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.janayalsalem.tipcalculatorapp.components.InputField
+import com.janayalsalem.tipcalculatorapp.components.RoundIconButton
 import com.janayalsalem.tipcalculatorapp.ui.theme.TipCalculatorAppTheme
 
 @ExperimentalComposeUiApi
@@ -31,14 +33,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppTheme{
-                TopHeader()
+            AppTheme {
                 MainContent() // need to use @ExperimentalComposeUiApi
             }
         }
     }
 }
-
 
 
 @Composable
@@ -51,89 +51,164 @@ fun AppTheme(content: @Composable () -> Unit) {
     }
 }
 
-@ExperimentalComposeUiApi
 @Composable
 fun TopHeader(totalPerPers: Double = 0.0) {
 
     val totalFormat = "%.2f".format(totalPerPers)
 
-    Surface(color = Color(0xFFe9d7f7)
-        ,modifier = Modifier
+    Surface(
+        color = Color(0xFFe9d7f7),
+        modifier = Modifier
             .fillMaxWidth()
             .height(150.dp)
             .padding(12.dp)
             .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
     ) {
-        Column(modifier = Modifier.padding(12.dp),
+        Column(
+            modifier = Modifier.padding(12.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center) {
+            verticalArrangement = Arrangement.Center
+        ) {
 
             Text(text = "Total Per Person", style = MaterialTheme.typography.subtitle1)
-            Text(text = "\$$totalFormat}", style = MaterialTheme.typography.h4)
+            Text(text = "\$$totalFormat", style = MaterialTheme.typography.h4)
 
         } // End Column
 
     } // End Surface
 
 }
+
 @ExperimentalComposeUiApi
 @Composable
 fun MainContent() {
 
-    Surface(modifier = Modifier
-        .padding(2.dp)
-        .fillMaxWidth(),
+    val splitBy = remember { mutableStateOf(1) }
+    val totalTipAmt = remember { mutableStateOf(0.0) }
+    val totalPerPerson = remember { mutableStateOf(0.0) }
+
+
+    Surface(
+        modifier = Modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
         shape = CircleShape.copy(all = CornerSize(8.dp)),
         border = BorderStroke(width = 1.dp, color = Color.LightGray)
     ) {
 
-        Column(modifier = Modifier.padding(6.dp),
+        Column(
+            modifier = Modifier.padding(6.dp),
             verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.Start) {
+            horizontalAlignment = Alignment.Start
+        ) {
+            TopHeader()
+            InputPriceField(splitByState = splitBy)
 
-          InputPriceField(){it}
-
-}
+        }
     }
+
 }
 
 @ExperimentalComposeUiApi
 @Composable
-fun InputPriceField(modifier: Modifier = Modifier, onValChange:(String)-> Unit = {}) {
+fun InputPriceField(
+    modifier: Modifier = Modifier,
+    splitByState: MutableState<Int>,
+    onValChange: (String) -> Unit = {},
+) {
 
     // What did number user enter.
     val totalNumber = remember { mutableStateOf("") }
 
     // LocalTextInputService.
-    val keyboardController = LocalSoftwareKeyboardController.current // need to use @ExperimentalComposeUiApi
+    val keyboardController =
+        LocalSoftwareKeyboardController.current // need to use @ExperimentalComposeUiApi
 
     // To check totalNumber isNotEmpty
     val valid = remember(totalNumber.value) { totalNumber.value.trim().isNotEmpty() }
 
-            InputField(
-                valueState = totalNumber, labelId = "Enter Number",
-                enabled = true,
-                onAction = KeyboardActions {
-                    //The submit button is disabled unless the inputs are valid. wrap this in if statement to accomplish the same.
-                    if (!valid) return@KeyboardActions
-                    onValChange(totalNumber.value.trim()) // get price
-                    //totalBill.value = ""
-                    keyboardController?.hide() //need to use @ExperimentalComposeUiApi
-                },
-            )
+    InputField(valueState = totalNumber, labelId = "Enter Number", enabled = true, onAction = KeyboardActions {
+            //The submit button is disabled unless the inputs are valid. wrap this in if statement to accomplish the same.
+            if (!valid) return@KeyboardActions
+            onValChange(totalNumber.value.trim()) // get price
+            //totalBill.value = ""
+            keyboardController?.hide() //need to use @ExperimentalComposeUiApi
+        },
+    )
+    val totalPerPerson = remember {
+        mutableStateOf(0.0)
+    }
 
+    // show a Split section if enter number
+    if (valid) {
+        Split(splitByState, totalNumber, totalPerPerson)
+    }
+}
+
+@Composable
+fun Split(
+          splitByState: MutableState<Int>,
+          totalNumber: MutableState<String>,
+          totalPerPersonState: MutableState<Double>,
+          range: IntRange = 1..100,) {
+
+    var sliderPosition by remember { mutableStateOf(0f) }
+    val tipPercentage = (sliderPosition * 100).toInt()
+
+
+        Row(modifier = Modifier.padding(3.dp), horizontalArrangement = Arrangement.Start) {
+            Text(
+                text = "Split",
+                modifier = Modifier.align(alignment = Alignment.CenterVertically)
+            )
+            Spacer(modifier = Modifier.width(120.dp))
+
+            Row(
+                modifier = Modifier.padding(horizontal = 3.dp),
+                horizontalArrangement = Arrangement.End
+            ) {
+
+                RoundIconButton(imageVector = Icons.Default.Remove, onClick = {
+                    splitByState.value =
+                        if (splitByState.value > 1) splitByState.value - 1 else 1
+                    totalPerPersonState.value = calculateTotalPerPerson(totalBill = totalNumber.value.toDouble(), splitBy = splitByState.value, tipPercent = tipPercentage)
+
+                })
+                Text(text = "${splitByState.value}",
+                    Modifier
+                        .align(alignment = Alignment.CenterVertically)
+                        .padding(start = 9.dp, end = 9.dp))
+                RoundIconButton(imageVector = Icons.Default.Add, onClick = {
+                    if (splitByState.value < range.last) {
+                        splitByState.value = splitByState.value + 1
+
+                        totalPerPersonState.value =
+                            calculateTotalPerPerson(totalBill = totalNumber.value.toDouble(),
+                                splitBy = splitByState.value,
+                                tipPercent = tipPercentage)
+                    }
+                })
+            }
+        }
 
 }
 
 
+fun calculateTotalTip(totalBill: Double, tipPercent: Int): Double {
+
+    return if (totalBill > 1 && totalBill.toString().isNotEmpty()) (totalBill * tipPercent) / 100 else 0.0
+}
+fun calculateTotalPerPerson(totalBill: Double, splitBy: Int, tipPercent: Int): Double {
+    val bill = calculateTotalTip(totalBill, tipPercent = tipPercent) + totalBill
+    return (bill/splitBy)
+}
 
 
 @ExperimentalComposeUiApi
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    AppTheme{
-//        TopHeader()
+    AppTheme {
         MainContent()
     }
 }
--- app/src/main/java/com/janayalsalem/tipcalculatorapp/components/Components.kt
+++ app/src/main/java/com/janayalsalem/tipcalculatorapp/components/Components.kt
@@ -1,13 +1,11 @@
 package com.janayalsalem.tipcalculatorapp.components
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AttachMoney
 import androidx.compose.material.icons.rounded.Email
@@ -16,9 +14,13 @@ import androidx.compose.material.icons.rounded.Phone
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -46,3 +48,26 @@ fun InputField(modifier: Modifier = Modifier,
         keyboardActions = onAction
     )
 } // end InputField
+
+
+
+@Composable
+fun RoundIconButton(
+    modifier: Modifier = Modifier,
+    imageVector: ImageVector,
+    onClick: () -> Unit,
+    tint: Color = Color.Black.copy(alpha = 0.8f),
+    backgroundColor: Color = MaterialTheme.colors.background,
+    elevation: Dp = 4.dp
+) {
+    Card(
+        modifier = modifier.padding(all = 4.dp)
+            .clickable(onClick = onClick)
+            .then(Modifier.size(40.dp)),
+        shape = CircleShape,
+        backgroundColor = backgroundColor,
+        elevation = elevation
+    ) {
+        Icon(imageVector, null, tint = tint)
+    }
+}
