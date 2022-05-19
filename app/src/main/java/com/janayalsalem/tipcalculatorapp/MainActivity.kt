d8b03ababa9a6df1bb2c158a0dc57670f184a067
Jana <Jana.Yousef.ruh.k@tuwaiq.edu.sa>
2022-05-19 07:38:04 +0300

    - Add input field

--- app/src/main/java/com/janayalsalem/tipcalculatorapp/MainActivity.kt
+++ app/src/main/java/com/janayalsalem/tipcalculatorapp/MainActivity.kt
@@ -3,32 +3,43 @@ package com.janayalsalem.tipcalculatorapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.janayalsalem.tipcalculatorapp.components.InputField
 import com.janayalsalem.tipcalculatorapp.ui.theme.TipCalculatorAppTheme
 
+//@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme{
                 TopHeader()
+//                MainContent() // need a @ExperimentalComposeUiApi
             }
         }
     }
 }
+
+
 
 @Composable
 fun AppTheme(content: @Composable () -> Unit) {
@@ -66,12 +77,53 @@ fun TopHeader(totalPerPers: Double = 0.0) {
 
 }
 
+@ExperimentalComposeUiApi
+@Composable
+fun MainContent() {
+
+    // What did number user enter.
+    val totalNumber = remember { mutableStateOf("") }
+
+    // LocalTextInputService.
+    val keyboardController = LocalSoftwareKeyboardController.current // need a @ExperimentalComposeUiApi
+
+    // To check totalNumber isNotEmpty
+    val valid = remember(totalNumber.value) { totalNumber.value.trim().isNotEmpty() }
+
+    Surface(modifier = Modifier
+        .padding(2.dp)
+        .fillMaxWidth(),
+        shape = CircleShape.copy(all = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
+    ) {
+
+        Column(modifier = Modifier.padding(6.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start) {
+
+            InputField(
+                valueState = totalNumber, labelId = "Enter Number",
+                enabled = true,
+                onAction = KeyboardActions {
+                    //The submit button is disabled unless the inputs are valid. wrap this in if statement to accomplish the same.
+                    if (!valid) return@KeyboardActions
+//                    onValChange(totalNumber.value.trim())
+                    //totalBill.value = ""
+                    keyboardController?.hide() //(to use this we need to use @ExperimentalComposeUiApi
+                },
+            )
+
+}
+    }
+}
 
 
+@ExperimentalComposeUiApi
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     AppTheme{
-        TopHeader()
+//        TopHeader()
+        MainContent()
     }
 }
